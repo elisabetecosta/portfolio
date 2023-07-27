1fa7aef6900e3f9475ce58e04de09f339fd628f7
Elisabete Costa <elisabete_costa94@hotmail.com>
2023-07-27 01:09:24 +0100

    removed other tech and added background image

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,4 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap"
+import backgroundImage from "../assets/img/color-sharp.png"
 import "./Tech.css"
 
 function Tech() {
@@ -20,13 +21,7 @@ function Tech() {
                                 <img src="https://www.vectorlogo.zone/logos/mongodb/mongodb-icon.svg" alt="MongoDB Logo" />
                                 <img src="https://cdn.worldvectorlogo.com/logos/typescript.svg" alt="Typescript Logo" />
                                 <img src="https://cdn.worldvectorlogo.com/logos/react-2.svg" alt="React Logo" />
-                            </div>
-
-                            <h2>Other Tech</h2>
-                            <p>Other technologies I work with</p>
-                            <div className="img-bx">
                                 <img src="https://cdn.worldvectorlogo.com/logos/mysql-logo.svg" alt="MySQL Logo" />
-                                <img src="https://cdn.worldvectorlogo.com/logos/react-native-1.svg" alt="React Native Logo" />
                                 <img src="https://cdn.worldvectorlogo.com/logos/wordpress-icon-1.svg" alt="Wordpress Logo" />
                                 <img src="https://cdn.worldvectorlogo.com/logos/php-1.svg" alt="PHP Logo" />
                             </div>
@@ -34,6 +29,8 @@ function Tech() {
                     </Col>
                 </Row>
             </Container>
+
+            <img src={backgroundImage} alt="" className="background-image-left" />
         </section>
     )
 }
