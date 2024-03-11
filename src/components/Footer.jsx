c5c2b0755bd22024ce45acad3a25442a0d53eca1
Elisabete Costa <elisabete_costa94@hotmail.com>
2024-03-11 18:15:39 +0000

    improve footer design on different screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,15 +11,15 @@ function Footer() {
         <footer className="footer">
             <Container>
                 <Row className="align-items-center">
-                    <Col sm={5}>
-                        <img src={logo} alt="Logo" />
+                    <Col xs={12} sm={7} md={4} className="text-center text-sm-start">
+                        <img src={logo} alt="Logo" className="img-fluid" />
                     </Col>
 
-                    <Col sm={4}>
+                    <Col xs={12} sm={5} md={4} className="text-center text-sm-end text-md-center">
                         <p>&copy; 2023 by Elisabete Costa</p>
                     </Col>
 
-                    <Col sm={3} className="text-center text-sm-end">
+                    <Col xs={12} md={4} className="text-center text-sm-end">
                         <div className="social-icons">
                             <a href="https://github.com/elisabetecosta" target="_blank" rel="noreferrer"><img src={navIcon1} alt="Github" /></a>
                             <a href="https://www.linkedin.com/in/elisabetecosta94" target="_blank" rel="noreferrer"><img src={navIcon2} alt="LinkedIn" /></a>
